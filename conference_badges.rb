4cf85b08b320f355b3114a365cb7ad0d05ef247a
Richard Chen <richard.c.chen@gmail.com>
2018-07-23 00:03:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,20 +3,10 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def conference_badges(array)
-  counter = 0
-  array.each do |i|
-    newarray[counter] = "Hello, my name is #{i}."
-    counter += 1
-  end
-newarray  
-end
-
-
 def batch_badge_creator(array)
   counter = 0
   array.each do |i|
-    newarray[counter] = "Hello, my name is #{i}."
+    newarray[counter] = badge_maker(i)
     counter += 1
   end
 newarray
