f05ed65a19d7375fa40fd375aa1654a5f94f1d94
Richard Chen <richard.c.chen@gmail.com>
2018-07-23 00:00:14 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,11 @@
 # Write your code here.
 def conference_badges(array)
-  
+  counter = 0
+  array.each do |i|
+    newarray[counter] = "Hello, my name is #{i}."
+    counter += 1
+  end
+newarray  
 end
 
 
