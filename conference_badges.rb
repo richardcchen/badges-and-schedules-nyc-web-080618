50c0319443db777841f8ebc9d752d110b2ae59f3
Richard Chen <richard.c.chen@gmail.com>
2018-07-23 00:17:39 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,5 +15,13 @@ newarray
 end
 
 def assign_rooms(array)
+  rooms =  [1, 2, 3, 4, 5, 6, 7]
+  counter = 0
+  newarray = []
+  array.each do |i|
+    newarray[counter] = "Hello, #{i}! You'll be assigned to room #{rooms[counter]}!"
+    counter += 1
+  end
+    
   
 end
